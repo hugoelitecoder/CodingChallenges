56cb0a1903cf07710cd75fb01c6c78142587f4c0
Hugo Steenhuis <h.steenhuis@aquadis.nl>
2025-09-02 04:48:37 +0200

    update code.

--- CodingGame/Solutions/Optim/2048.cs
+++ CodingGame/Solutions/Optim/2048.cs
@@ -1,191 +1,198 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 public class Solution
 {
-    const int BOARD_SIZE = 4;
-    static readonly int[,] BLANK_GRID = new int[BOARD_SIZE, BOARD_SIZE];
-    const int BEAM_WIDTH = 40;
-    const int FIRST_TURN_BEAM_WIDTH = 50;
-    static readonly char[] POSSIBLE_MOVE = { 'U', 'D', 'L', 'R' };
-    static readonly double[] POIDS = { 97.30119, 90.68706, 13.228226, 67.991486, 12.232829 };
-    const int MAX_DEPTH = 60;
-    const int FIRST_TURN_MAX_DEPTH = 1500;
-    const double SAFE_PATH = 1.5;
+    // --- Performance Tuning Parameters ---
+    const int NORMAL_TURN_BEAM_WIDTH = 60;
+    const int FIRST_TURN_BEAM_WIDTH = 100;
+    const int ABSOLUTE_MAX_DEPTH = 1200;
+    const double SAFE_PATH_RATIO = 1.5;
 
-    static bool firstTurn = true;
+    // --- Game Logic Constants ---
+    const int BOARD_SIZE = 4;
+    static readonly char[] POSSIBLE_MOVES = { 'U', 'D', 'L', 'R' };
+    static readonly double[] HEURISTIC_WEIGHTS = { 97.30119, 90.68706, 13.228226, 67.991486, 12.232829 };
+
+    // --- State ---
+    static bool _isFirstTurn = true;
+    private static readonly int[] _freeCellBuffer = new int[BOARD_SIZE * BOARD_SIZE];
 
     public static void Main(string[] args)
     {
         while (true)
         {
-            var seedLine = Console.ReadLine();
+            string seedLine = Console.ReadLine();
             if (seedLine == null) return;
-            var seed = long.Parse(seedLine);
-            var score = int.Parse(Console.ReadLine());
+
+            long seed = long.Parse(seedLine);
+            int.Parse(Console.ReadLine());
             var grid = new int[BOARD_SIZE, BOARD_SIZE];
             for (var i = 0; i < BOARD_SIZE; i++)
             {
                 var inputs = Console.ReadLine().Split(' ');
                 for (var j = 0; j < BOARD_SIZE; j++) grid[i, j] = int.Parse(inputs[j]);
             }
-            var m = BestMove(grid, seed);
-            firstTurn = false;
-            Console.WriteLine(m);
+            var moveSequence = FindBestMoveSequence(grid, seed);
+            _isFirstTurn = false;
+            Console.WriteLine(moveSequence);
         }
     }
 
-    class TreeGrid
+    struct SearchNode
     {
         public int[,] grid;
         public long seed;
-        public int score;
+        public int scoreGain;
         public int depth;
         public string path;
-        public int margedCell;
+        public int mergedCells;
         public int biggestCell;
-        public double heuristique;
-        public TreeGrid(int[,] grid, long seed, int score, int depth, string path, int margedCell, int biggestCell)
+        public double heuristicScore;
+
+        public SearchNode(int[,] grid, long seed, int scoreGain, int depth, string path, int mergedCells, int biggestCell)
         {
             this.grid = grid;
             this.seed = seed;
-            this.score = score;
+            this.scoreGain = scoreGain;
             this.depth = depth;
             this.path = path;
-            this.margedCell = margedCell;
+            this.mergedCells = mergedCells;
             this.biggestCell = biggestCell;
-            this.heuristique = Heuristique(this);
+            this.heuristicScore = CalculateHeuristicScore(this);
         }
     }
 
-    static string BestMove(int[,] grid, long seed)
+    static string FindBestMoveSequence(int[,] grid, long seed)
     {
-        var nodes = new List<TreeGrid> { new TreeGrid(Clone(grid), seed, 0, 0, "", 0, 0) };
-        var maxDepth = firstTurn ? FIRST_TURN_MAX_DEPTH : MAX_DEPTH;
-        while (nodes[0].depth != maxDepth)
+        var stopwatch = Stopwatch.StartNew();
+        long timeLimitMs = _isFirstTurn ? 950 : 45;
+
+        var currentBeam = new List<SearchNode> { new SearchNode(Clone(grid), seed, 0, 0, "", 0, 0) };
+        string bestPathFound = "U";
+
+        for (int depth = 0; depth < ABSOLUTE_MAX_DEPTH; depth++)
         {
-            var next = new List<TreeGrid>();
-            var n = firstTurn ? FIRST_TURN_BEAM_WIDTH : BEAM_WIDTH;
-            var take = Math.Min(nodes.Count, n);
-            for (var i = 0; i < take; i++)
+            var nextBeam = new List<SearchNode>();
+            int beamWidth = _isFirstTurn ? FIRST_TURN_BEAM_WIDTH : NORMAL_TURN_BEAM_WIDTH;
+            int nodesToExpand = Math.Min(currentBeam.Count, beamWidth);
+
+            for (var i = 0; i < nodesToExpand; i++)
             {
-                var node = nodes[i];
-                for (var iMove = 0; iMove < 4; iMove++)
+                var node = currentBeam[i];
+                foreach (var move in POSSIBLE_MOVES)
                 {
-                    var res = Move(node.grid, POSSIBLE_MOVE[iMove]);
-                    var _grid = res.grid;
-                    var gained = res.score;
-                    var isSameGrid = res.isSameGrid;
-                    var merged = res.margedCell;
-                    var biggest = res.biggestCell;
-                    if (!isSameGrid)
+                    var moveResult = Move(node.grid, move);
+                    if (!moveResult.isSameGrid)
                     {
-                        var preshot = PreshotSpawn(_grid, node.seed);
-                        if (!double.IsNaN(preshot.x) && !double.IsNaN(preshot.y))
+                        var spawnedGrid = moveResult.grid;
+                        var nextTileSpawn = GetNextTileSpawn(spawnedGrid, node.seed);
+                        if (!double.IsNaN(nextTileSpawn.x))
                         {
-                            _grid[(int)preshot.x, (int)preshot.y] = preshot.value;
+                            spawnedGrid[(int)nextTileSpawn.x, (int)nextTileSpawn.y] = nextTileSpawn.value;
                         }
-                        var path = node.path + POSSIBLE_MOVE[iMove];
-                        var child = new TreeGrid(_grid, preshot.seed, gained, node.depth + 1, path, merged, biggest);
-                        next.Add(child);
+                        var path = node.path + move;
+                        var child = new SearchNode(spawnedGrid, nextTileSpawn.nextSeed, moveResult.score, node.depth + 1, path, moveResult.mergedCells, moveResult.biggestCell);
+                        nextBeam.Add(child);
                     }
                 }
             }
-            if (next.Count == 0)
-            {
-                if (nodes[0].path == "") nodes[0].path = "U";
-                break;
-            }
-            next.Sort((a, b) => b.heuristique.CompareTo(a.heuristique));
-            nodes = next;
+
+            if (nextBeam.Count == 0) break;
+
+            nextBeam.Sort((a, b) => b.heuristicScore.CompareTo(a.heuristicScore));
+            currentBeam = nextBeam;
+            bestPathFound = currentBeam[0].path;
+
+            if (stopwatch.ElapsedMilliseconds > timeLimitMs) break;
         }
-        var pathOut = nodes[0].path;
-        var cut = (int)Math.Round(pathOut.Length / SAFE_PATH);
-        if (cut < 0) cut = 0;
-        if (cut > pathOut.Length) cut = pathOut.Length;
-        return pathOut.Substring(0, cut);
+
+        var cut = (int)Math.Round(bestPathFound.Length / SAFE_PATH_RATIO);
+        if (cut <= 0) cut = 1;
+        if (cut > bestPathFound.Length) cut = bestPathFound.Length;
+        return bestPathFound.Substring(0, cut);
     }
 
-    static (int[,] grid, int score, bool isSameGrid, int margedCell, int biggestCell) Move(int[,] grid, char m)
+    static (int[,] grid, int score, bool isSameGrid, int mergedCells, int biggestCell) Move(int[,] grid, char m)
     {
         var sameGrid = true;
         var _grid = Clone(grid);
         var score = 0;
-        var margedCell = 0;
+        var mergedCells = 0;
         var biggestCell = 0;
 
         var indexStart = (m == 'U' || m == 'L') ? 0 : BOARD_SIZE - 1;
         var indexEnd = (m == 'U' || m == 'L') ? BOARD_SIZE - 1 : 0;
         var indexInc = (m == 'U' || m == 'L') ? 1 : -1;
 
-        for (var i = indexStart; i != indexEnd + indexInc; i += indexInc)
+        for (var i = 0; i < BOARD_SIZE; i++)
         {
             for (var j = indexStart; j != indexEnd + indexInc; j += indexInc)
             {
-                var index1 = (m == 'U' || m == 'D') ? i : j;
-                var index2 = (m == 'U' || m == 'D') ? j : i;
-                var end = indexEnd + indexInc;
+                var index1 = (m == 'U' || m == 'D') ? j : i;
+                var index2 = (m == 'U' || m == 'D') ? i : j;
 
-                var k = ((m == 'U' || m == 'D') ? index1 : index2) + indexInc;
+                if (_grid[index1, index2] == 0) continue;
 
-                if (_grid[index1, index2] == 0)
+                var k = j + indexInc;
+                while (k >= 0 && k < BOARD_SIZE)
                 {
-                    for (; k != end; k += indexInc)
-                    {
-                        var kIndex1 = (m == 'U' || m == 'D') ? k : index1;
-                        var kIndex2 = (m == 'U' || m == 'D') ? index2 : k;
-                        if (_grid[kIndex1, kIndex2] != 0)
-                        {
-                            sameGrid = false;
-                            _grid[index1, index2] = _grid[kIndex1, kIndex2];
-                            _grid[kIndex1, kIndex2] = 0;
-                            break;
-                        }
-                    }
-                }
+                    var kIndex1 = (m == 'U' || m == 'D') ? k : i;
+                    var kIndex2 = (m == 'U' || m == 'D') ? i : k;
 
-                if (_grid[index1, index2] != 0)
+                    if (_grid[kIndex1, kIndex2] == 0) { k += indexInc; continue; }
+                    if (_grid[kIndex1, kIndex2] == _grid[index1, index2])
+                    {
+                        var newval = _grid[index1, index2] * 2;
+                        _grid[index1, index2] = newval;
+                        _grid[kIndex1, kIndex2] = 0;
+                        score += newval;
+                        mergedCells++;
+                        sameGrid = false;
+                    }
+                    break;
+                }
+            }
+
+            for (var j = indexStart; j != indexEnd + indexInc; j += indexInc)
+            {
+                var index1 = (m == 'U' || m == 'D') ? j : i;
+                var index2 = (m == 'U' || m == 'D') ? i : j;
+
+                if (_grid[index1, index2] != 0) continue;
+
+                var k = j + indexInc;
+                while (k >= 0 && k < BOARD_SIZE)
                 {
-                    for (; k != end; k += indexInc)
+                    var kIndex1 = (m == 'U' || m == 'D') ? k : i;
+                    var kIndex2 = (m == 'U' || m == 'D') ? i : k;
+                    if (_grid[kIndex1, kIndex2] != 0)
                     {
-                        var kIndex1 = (m == 'U' || m == 'D') ? k : index1;
-                        var kIndex2 = (m == 'U' || m == 'D') ? index2 : k;
-                        if (_grid[kIndex1, kIndex2] == _grid[index1, index2])
-                        {
-                            sameGrid = false;
-                            var cellNumber = _grid[index1, index2] * 2;
-                            _grid[index1, index2] = cellNumber;
-                            _grid[kIndex1, kIndex2] = 0;
-                            score += cellNumber;
-                            margedCell++;
-                            break;
-                        }
-                        else if (_grid[kIndex1, kIndex2] != 0)
-                        {
-                            break;
-                        }
+                        _grid[index1, index2] = _grid[kIndex1, kIndex2];
+                        _grid[kIndex1, kIndex2] = 0;
+                        sameGrid = false;
+                        break;
                     }
+                    k += indexInc;
                 }
-
-                if (_grid[index1, index2] > biggestCell) biggestCell = _grid[index1, index2];
             }
         }
 
-        return (_grid, score, sameGrid, margedCell, biggestCell);
+        for (int i = 0; i < BOARD_SIZE; i++) for (int j = 0; j < BOARD_SIZE; j++) if (_grid[i, j] > biggestCell) biggestCell = _grid[i, j];
+
+        return (_grid, score, sameGrid, mergedCells, biggestCell);
     }
 
-    static (double x, double y, int value, long seed) PreshotSpawn(int[,] grid, long seed)
+    static (double x, double y, int value, long nextSeed) GetNextTileSpawn(int[,] grid, long seed)
     {
-        var free = new List<int>();
+        int freeCellCount = 0;
         for (var y = 0; y < BOARD_SIZE; y++)
-        {
             for (var x = 0; x < BOARD_SIZE; x++)
-            {
-                if (grid[x, y] == 0) free.Add(x + y * BOARD_SIZE);
-            }
-        }
-        if (free.Count == 0) return (double.NaN, double.NaN, 0, seed);
-        var spawnIndex = free[(int)(seed % free.Count)];
+                if (grid[x, y] == 0) _freeCellBuffer[freeCellCount++] = x + y * BOARD_SIZE;
+
+        if (freeCellCount == 0) return (double.NaN, double.NaN, 0, seed);
+        var spawnIndex = _freeCellBuffer[(int)(seed % freeCellCount)];
         var value = ((seed & 0x10) == 0) ? 2 : 4;
         var sx = spawnIndex % BOARD_SIZE;
         var sy = spawnIndex / BOARD_SIZE;
@@ -193,40 +200,33 @@ public class Solution
         return (sx, sy, value, nextSeed);
     }
 
-    static double Heuristique(TreeGrid node)
+    static double CalculateHeuristicScore(SearchNode node)
     {
         var grid = node.grid;
-        var monotonie = CalculMonotonie(grid);
-        var score = node.score * POIDS[0] + node.margedCell * POIDS[1] + node.biggestCell * POIDS[3];
-        score -= monotonie * POIDS[4];
-        if (grid[0, 0] == node.biggestCell || grid[0, 3] == node.biggestCell || grid[3, 0] == node.biggestCell || grid[3, 3] == node.biggestCell) score *= POIDS[2];
+        var monotonicity = CalculateMonotonicity(grid);
+        var score = node.scoreGain * HEURISTIC_WEIGHTS[0] + node.mergedCells * HEURISTIC_WEIGHTS[1] + node.biggestCell * HEURISTIC_WEIGHTS[3];
+        score -= monotonicity * HEURISTIC_WEIGHTS[4];
+        if (grid[0, 0] == node.biggestCell || grid[0, 3] == node.biggestCell || grid[3, 0] == node.biggestCell || grid[3, 3] == node.biggestCell) score *= HEURISTIC_WEIGHTS[2];
         return score;
     }
 
-    static int CalculMonotonie(int[,] grid)
+    static int CalculateMonotonicity(int[,] grid)
     {
-        var monotonie = 0;
+        var monotonicity = 0;
         for (var i = 0; i < BOARD_SIZE; i++)
         {
-            var rowMonotonie = 0;
             for (var j = 0; j < BOARD_SIZE - 1; j++)
             {
-                var diff = Math.Abs(grid[i, j] - grid[i, j + 1]);
-                rowMonotonie += diff;
+                monotonicity += Math.Abs(grid[i, j] - grid[i, j + 1]);
             }
-            monotonie += rowMonotonie;
         }
-        return monotonie;
+        return monotonicity;
     }
 
     static int[,] Clone(int[,] src)
     {
-        var r = src.GetLength(0);
-        var c = src.GetLength(1);
-        var dst = new int[r, c];
-        for (var i = 0; i < r; i++)
-            for (var j = 0; j < c; j++)
-                dst[i, j] = src[i, j];
+        var dst = new int[BOARD_SIZE, BOARD_SIZE];
+        Array.Copy(src, dst, src.Length);
         return dst;
     }
 }
